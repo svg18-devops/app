7a99e7f3c4765b41fbb3826b2d9f8d7e17500bab
svg18-devops <84924935+svg18-devops@users.noreply.github.com>
2025-11-20 19:27:08 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const os = require('os');
+
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(`Hello from node ${os.hostname()}\n`);
+});
+
+server.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
